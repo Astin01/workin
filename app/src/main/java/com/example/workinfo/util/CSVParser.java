f9d4cd997b1781ac92c9d7b17f08bf3dfd837128
Donghun Won <wonhun1225@gmail.com>
2023-12-15 01:19:18 +0900

    feat : 그래프 기능 추가

--- app/src/main/java/com/example/workinfo/util/CSVParser.java
+++ app/src/main/java/com/example/workinfo/util/CSVParser.java
@@ -4,6 +4,11 @@ import android.content.Context;
 
 import com.example.workinfo.R;
 import com.example.workinfo.data.FirstCompany;
+import com.example.workinfo.data.Job;
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
 import com.opencsv.bean.CsvToBeanBuilder;
 
 import java.io.BufferedReader;
@@ -43,5 +48,55 @@ public class CSVParser {
         is.close();
 
         return companyList;
+    }
+
+    public List<Job> getJobList(Context context) throws IOException {
+        InputStream is = context.getResources().openRawResource(R.raw.job);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8));
+
+        String line = "";
+
+        List<Job> jobList = new ArrayList<>();
+
+        while ((line = reader.readLine()) != null) {
+            String[] tokens = line.split(",");    // 각 행을 순차적으로 돌면서 , 를 기준으로 분리하여 배열에 저장한다.
+
+            Job job = new Job();
+            job.setDate(tokens[0]);
+            job.setTotal(tokens[1]);
+            job.setPeople(tokens[2]);
+            job.setSucess(tokens[3]);
+
+            jobList.add(job);
+        }
+
+        reader.close();
+        is.close();
+
+        return jobList;
+    }
+    public void setChart(List<Job> jobList, BarChart barChart){
+        List<BarEntry> entriesGroup1 = new ArrayList<>();
+        List<BarEntry> entriesGroup2 = new ArrayList<>();
+        List<BarEntry> entriesGroup3 = new ArrayList<>();
+
+        for(int i = 1; i < jobList.size(); i++) {
+            entriesGroup1.add(new BarEntry(i, Integer.parseInt(jobList.get(i).getTotal())));
+            entriesGroup2.add(new BarEntry(i, Integer.parseInt(jobList.get(i).getPeople())));
+            entriesGroup3.add(new BarEntry(i, Integer.parseInt(jobList.get(i).getSucess())));
+        }
+        BarDataSet set1 = new BarDataSet(entriesGroup1, "구직인원");
+        BarDataSet set2 = new BarDataSet(entriesGroup2, "구직자수");
+        BarDataSet set3 = new BarDataSet(entriesGroup3, "취업건수");
+
+        float groupSpace = 0.06f;
+        float barSpace = 0.02f; // x2 dataset
+        float barWidth = 0.45f; // x2 dataset
+// (0.02 + 0.45) * 2 + 0.06 = 1.00 -> interval per "group"
+        BarData data = new BarData(set1, set2,set3);
+        data.setBarWidth(barWidth); // set the width of each bar
+        barChart.setData(data);
+        barChart.groupBars(0, groupSpace, barSpace); // perform the "explicit" grouping
+        barChart.invalidate(); // refresh
     }
 }
